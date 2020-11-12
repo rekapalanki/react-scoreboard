dd1af91d811b4512660ea20c0f53c6d5c6c2bf4e
Reka Palanki <reka.palanki@gmail.com>
2020-11-12 14:47:30 +0000

    creating layout and styles

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,7 @@ import Counterboard from "./Counterboard";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello Counterboard!</h1>
-      <Counterboard />
-    </div>
+    <Counterboard />
   )
 }
 
--- src/components/Counterboard.js
+++ src/components/Counterboard.js
@@ -3,15 +3,29 @@ import useState from "react";
 
 const Counterboard = () => {
   const [count, setCount] = React.useState(0);
+  const [count2, setCount2] = React.useState(0);
 
   return (
-    <div>
-      <h2>Home</h2>
-      <div id="counter-box" onClick={() => setCount(count + 1)}></div>
-      <h2>Visitor</h2>
-      <div id="counter-box" onClick={() => setCount(count + 1)}></div>
+    <div className="wrapper">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+      <div className="container">
+        <div id="box1">
+          <h2>Home</h2>
+          <div className="counter-box" onClick={() => setCount(count + 1)}>
+          {count}
+          </div>
+        </div>
+        <div id="box2">
+          <h2>Visitor</h2>
+          <div className="counter-box" onClick={() => setCount2(count2 + 1)}>
+          {count2}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Counterboard;

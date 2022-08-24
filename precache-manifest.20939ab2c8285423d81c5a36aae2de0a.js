c9173bec47bb5ca6b1e6edd1028a0abe5f14ec23
Reka Palanki <reka.palanki@gmail.com>
2022-08-24 09:55:48 +0100

    Updates

---
+++ precache-manifest.20939ab2c8285423d81c5a36aae2de0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5037ed3473b1133ee601ca5db8947703",
+    "url": "/react-scoreboard/index.html"
+  },
+  {
+    "revision": "70168c10d8500928ef48",
+    "url": "/react-scoreboard/static/css/main.ea63f09a.chunk.css"
+  },
+  {
+    "revision": "0141a74df5c3a50d31e1",
+    "url": "/react-scoreboard/static/js/2.f8ff514e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-scoreboard/static/js/2.f8ff514e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70168c10d8500928ef48",
+    "url": "/react-scoreboard/static/js/main.98284c39.chunk.js"
+  },
+  {
+    "revision": "b608536ad2aa96537327",
+    "url": "/react-scoreboard/static/js/runtime-main.a01301ba.js"
+  },
+  {
+    "revision": "a6509aa3161205ebe01c0f7e2f209b44",
+    "url": "/react-scoreboard/static/media/LeagueSpartan-Bold.a6509aa3.otf"
+  },
+  {
+    "revision": "bece844daeb17fd2add8f3fc407c4126",
+    "url": "/react-scoreboard/static/media/zero_and_zero_is.bece844d.ttf"
+  }
+]);
